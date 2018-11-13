0e8f34f4287513f26af3e40ef76772f8720c1fdf
ngamel <ngamel@r30.fdsetu.infra.umontpellier.fr>
2018-11-13 12:37:15 +0100

    changement de l'affichage de la fenêtre

--- projetVCFGraphique.py
+++ projetVCFGraphique.py
@@ -2,20 +2,44 @@
 
 import projetVCF as vcf
 
-from tkinter import * 
+import tkinter as tk
+import tkinter.scrolledtext as tkst
 
-fenetre = Tk()
+fenetre = tk.Tk()
 
+"""
 label = Label(fenetre, text="Hello World")
 label.pack()
+"""
+
+frame1 = tk.Frame(
+    master = fenetre,
+    bg = '#808000'
+)
+
+frame1.pack(fill='both', expand='yes')
+
+editArea = tkst.ScrolledText(
+    master = frame1,
+    wrap   = tk.WORD,
+    width  = 20,
+    height = 10
+)
+
+editArea.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+
 
 for l in vcf.dico:
-	value = StringVar()
+	value = tk.StringVar()
 
 	value.set(l + " " + str(vcf.dico[l]))
 
+	editArea.insert(tk.INSERT,value)
+"""
 	entree = Entry(fenetre, textvariable=value, width=200)
 	entree.pack()
+"""
 
 """
 canvas = Canvas(fenetre, width=150, height=120)
